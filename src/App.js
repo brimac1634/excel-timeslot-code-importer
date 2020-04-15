8c86626b84cd5c0abebdc7d37c728ae6b59f08af
Brian MacPherson <brimac1634@gmail.com>
2020-04-15 14:42:10 +0800

    added base and number of random characters input along with checkboxes for alph and num

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import './App.scss';
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [settings, setSettings] = useState({
+    base: '',
+    numOfRandom: 4,
+    useAlpha: true,
+    useNum: true
+  })
+
+  const { base, numOfRandom, useAlpha, useNum } = settings;
+
+  const handleSettingsChange = e => {
+    setSettings({ ...settings, [e.target.name]: e.target.value })
+  }
+
+  const handleCheck = e => {
+    setSettings({ ...settings, [e.target.name]: !settings[e.target.name] })
+  }
+  console.log(settings);
 
   const handleChangeFile = e => {
     const file = e.target.files[0];
@@ -38,7 +55,7 @@ const App = () => {
 
         let randomString = ''
         for (var i = 0; i < numOfRandom; i++) {
-          if (useAlpha && useNum) {
+          if ((useAlpha && useNum) || (!useAlpha && !useNum)) {
             randomString += String(alphaNum[Math.floor(alphaNum.length * Math.random())]);
           } else if (useAlpha && !useNum) {
             randomString += String(alpha[Math.floor(alpha.length * Math.random())]);
@@ -49,32 +66,102 @@ const App = () => {
         if (base) return base + randomString;
         return randomString;
       }
+      console.log(rows)
       
-      const newRows = rows.map(({ date, start, end }) => {
-        
-        console.log(date, start)
+      const newRows = rows.map(({ code, date, start, end }) => {
         return {
-          code: generateCode('hey', 4, false, true),
+          code: code ? code : generateCode(base, numOfRandom, useAlpha, useNum),
           start: new Date(parseDateExcel(date + start)),
           end: new Date(parseDateExcel(date + end))
         }
       })
-      console.log(newRows);
+
       setData(newRows);
     }
   }
 
+  const dateOptions = {
+    year: '2-digit',
+    month:'2-digit',
+    day:'2-digit',
+    hour12 : true,
+    hour:  '2-digit',
+    minute: '2-digit',
+  }
+
   return (
     <div className='app'>
-      <input 
-        className='input-file'
-        type='file' 
-        name='file'
-        id='upload' 
-        onChange={handleChangeFile} 
-        required
-      />
-      <label htmlFor='upload'>Choose File</label>
+      <div className='section'>
+        <span className='step'>1.</span>
+        <div className='group'>
+          <input 
+            className='form-input'
+            type='text' 
+            name='base'
+            id='base' 
+            value={base}
+            onChange={handleSettingsChange}
+          />
+          <label 
+            htmlFor='base' 
+            className='form-input-label'
+          >
+            Entry Code Base (optional)
+          </label>
+        </div>
+        <div className='col'>
+          <div className='group'>
+            <input 
+              className='form-input'
+              type='number' 
+              name='numOfRandom'
+              id='numOfRandom' 
+              value={numOfRandom}
+              onChange={handleSettingsChange}
+            />
+            <label 
+              htmlFor='numOfRandom' 
+              className='form-input-label'
+            >
+              Number of Random Characters
+            </label>
+          </div>
+        </div>
+        <div className='col'>
+          <div className='row'>
+            <input 
+              type='checkbox'
+              id='useAlpha'
+              name='useAlpha'
+              checked={useAlpha}
+              onChange={handleCheck}
+            />
+            <label htmlFor='useAlpha'>Use Alphabet</label>
+          </div>
+          <div className='row'>
+            <input 
+              type='checkbox'
+              id='useNum'
+              name='useNum'
+              checked={useNum}
+              onChange={handleCheck}
+            />
+            <label htmlFor='useNum'>Use Numbers</label>
+          </div>
+        </div>
+      </div>
+      <div className='section'>
+        <span className='step'>2.</span>
+        <input 
+          className='input-file'
+          type='file' 
+          name='file'
+          id='upload' 
+          onChange={handleChangeFile} 
+          required
+        />
+        <label htmlFor='upload'>Choose File</label>
+      </div>
       <div className='table'>
         <div className='row header'>
           <span>Entry Code</span>
@@ -87,8 +174,9 @@ const App = () => {
           data.map(({ code, start, end }, i) => (
             <div key={i} className='row'>
               <span>{code}</span>
-              <span>{start.toString().slice()}</span>
-              <span>{end.toString()}</span>
+              <span>{start.toLocaleDateString('en-US', dateOptions)}</span>
+              <span>{end.toLocaleDateString('en-US', dateOptions)}</span>
+              <span>{start.toLocaleDateString('en-US', { timeZoneName: 'short' })}</span>
             </div>
           ))
         }
